6fcb598790cbe4057586dbe0fb3afd9f1aa792c6
sicommnend <sicommnend@users.noreply.github.com>
2016-02-27 21:03:13 -0700

    Removed ignore from install
    Removed IGNORE from install for compatability reasons and mod approval
    at SM.

--- install.php
+++ install.php
@@ -5,5 +5,5 @@ if (!defined('SMF'))
 add_integration_function('integrate_pre_include','$sourcedir/og.php');
 add_integration_function('integrate_bbc_codes','og_bbc');
 add_integration_function('integrate_modify_modifications','og_admin');
-$smcFunc['db_query']('', "INSERT IGNORE INTO {db_prefix}scheduled_tasks (time_offset, time_regularity, time_unit, disabled, task) VALUES ('0', '1', 'w', '0', 'og_prune')");
+$smcFunc['db_query']('', "INSERT INTO {db_prefix}scheduled_tasks (time_offset, time_regularity, time_unit, disabled, task) VALUES ('0', '1', 'w', '0', 'og_prune')");
 ?>
